7a51253e6bbb08bbe17ddd3cdd69cbb447c296df
DESKTOP-4LD26K8\Kostas <koskonstant@gmail.com>
2018-04-19 12:56:20 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+var browserSync = require('browser-sync').create();
+
+// Compile SCSS
+gulp.task('css:compile', function() {
+  return gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass.sync({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'))
+});
+
+// Minify CSS
+gulp.task('css:minify', ['css:compile'], function() {
+  return gulp.src([
+      './assets/css/*.css',
+      '!./assets/css/*.min.css'
+    ])
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(browserSync.stream());
+});
+
+// CSS
+gulp.task('css', ['css:compile', 'css:minify']);
+
+// Default task
+gulp.task('default', ['css']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+// Dev task
+gulp.task('dev', ['css', 'browserSync'], function() {
+  gulp.watch('./assets/scss/*.scss', ['css']);
+  gulp.watch('./*.html', browserSync.reload);
+});;
